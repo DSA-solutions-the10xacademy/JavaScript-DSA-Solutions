695bb3ef5d4ce90aa841523dac1920c7d40b6eb3
Abdulahad Khan <43092512+Abduliitr@users.noreply.github.com>
2022-02-26 15:31:55 +0530

    Arithmetic - Geometric Progression (#4)
    * Practice problem - math - geometricProgression
    
    * Correction in naming conventions
    
    * Updated topic name as per request

---
+++ solutions/arithmetic/geometric-progression.js
@@ -0,0 +1,21 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+  idx++;
+  return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
+
+let firstNumber = parseInt(readLine());
+let secondNumber = parseInt(readLine());
+let thirdNumber = parseInt(readLine());
+
+let commonRatio = secondNumber/firstNumber;
+let fourthNumber = commonRatio * thirdNumber;
+
+console.log(parseInt(fourthNumber));
