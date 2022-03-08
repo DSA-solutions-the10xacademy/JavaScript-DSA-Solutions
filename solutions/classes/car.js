edd922f8d42ad41723e360386941623cb910343c
Rahul kumar <rahul.ku.g@gmail.com>
2022-03-08 16:21:38 +0530

    car

--- solutions/classes/car.js
+++ solutions/classes/car.js
@@ -1,29 +1,31 @@
 let fs = require("fs");
-let data = fs.readFileSync(0, 'utf-8');
+let data = fs.readFileSync(0, "utf-8");
 let idx = 0;
-data = data.split('\n');
+data = data.split("\n");
 
 function readLine() {
-	idx++;
-	return data[idx - 1];
+  idx++;
+  return data[idx - 1];
 }
 
 // -------- Do NOT edit anything above this line ----------
-// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
-const Flight = class{
-	constructor(upTime,downTime){
-		this.upTime=upTime;
-		this.downTime=downTime;
-	}
-	calculateFlight(){
-		var upTimeArr=this.upTime.split(':');
-		upTimeArr=upTimeArr.map((x)=>parseInt(x));
-		var downTimeArr=this.downTime.split(':');
-		downTimeArr=downTimeArr.map((x)=>parseInt(x));
-		return (downTimeArr[0]-upTimeArr[0])*60 + downTimeArr[1]-upTimeArr[1];
-	}
+
+// Define the required class here...
+const Car = class {
+  constructor(name = "Audi", model = "A4") {
+    this.name = name;
+    this.model = model;
+  }
+};
+
+// DO NOT CHANGE ANYTHING BELOW THIS LINE
+let flag = parseInt(readLine());
+if (flag === 1) {
+  var input_string = readLine().split(" ");
+  p1 = new Car(input_string[0], input_string[1]);
+} else {
+  p1 = new Car();
 }
 
-upTime=readLine();
-downTime=readLine();
-console.log(new Flight(upTime,downTime).calculateFlight());
+console.log(p1.name);
+console.log(p1.model);
