50a2dfbddbb736e02148b1b8e4872936f93c06a2
Abduliitr <abdul.thomso@gmail.com>
2022-03-09 11:20:27 +0530

    Updated with latest template

--- solutions/functions/min-max-range.js
+++ solutions/functions/min-max-range.js
@@ -11,11 +11,13 @@ function readLine() {
 
 // ## This function should return single integer. The integer should be maximum value of input list
 // ## Please fill the following function
-function maximumValue(input_numbers){
+function maximumValue(inputNumbers){
     // write below this here
     let maxValue = Number.NEGATIVE_INFINITY;
-    for(let i = 0; i < input_numbers.length; i++){
-        if(input_numbers[i] > maxValue) maxValue = input_numbers[i];
+    for(let i = 0; i < inputNumbers.length; i++){
+        if(inputNumbers[i] > maxValue){
+            maxValue = inputNumbers[i];
+        }
     }
 
     return maxValue;
@@ -23,11 +25,13 @@ function maximumValue(input_numbers){
 
 // ## This function should return single integer. 
 // ## The integer should be minimum value of input list
-function minimumValue(input_numbers){
+function minimumValue(inputNumbers){
     // Please write below this
     let minValue = Number.POSITIVE_INFINITY;
-    for(let i = 0; i < input_numbers.length; i++){
-        if(input_numbers[i] < minValue) minValue = input_numbers[i];
+    for(let i = 0; i < inputNumbers.length; i++){
+        if(inputNumbers[i] < minValue){ 
+            minValue = inputNumbers[i];
+        }
     }
 
     return minValue;
@@ -37,15 +41,15 @@ function minimumValue(input_numbers){
 // ## This function should return list of integer which lies between m1 and m2. 
 // ## if m1 <= m2 return list off numbers between m1 and m2
 // ## if m2 <= m1 return list off numbers between m2 and m1
-function getNumbersInRange(input_numbers, m1, m2){
+function getNumbersInRange(inputNumbers, m1, m2){
     // Please write below this line
     let minRange = Math.min(m1, m2);
     let maxRange = Math.max(m1, m2);
 
     let result = [];
-    for(let i = 0; i < input_numbers.length; i++){
-        if(input_numbers[i] >= minRange && input_numbers[i] <= maxRange){
-            result.push(input_numbers[i]);
+    for(let i = 0; i < inputNumbers.length; i++){
+        if(inputNumbers[i] >= minRange && inputNumbers[i] <= maxRange){
+            result.push(inputNumbers[i]);
         }
     }
 
@@ -59,18 +63,18 @@ function getNumbersInRange(input_numbers, m1, m2){
 //  Please do not change anything below this line.
 function ConvertToNumber(list){
     for(let each in list){
-        list[each]=Number(list[each])
+      list[each]=Number(list[each]);
     }
-    return list
+    return list;
 }
 
-let list1 = readLine().split(" ")
-let list2 = readLine().split(" ")
-let list3 = readLine().split(" ")   
-ConvertToNumber(list1)
-ConvertToNumber(list2)
-ConvertToNumber(list3)
-const m1 = minimumValue(list1)
-const m2 = maximumValue(list2)
-let minMaxRange = getNumbersInRange(list3, m1, m2)
-console.log(...minMaxRange)
+let list1 = readLine().split(" ");
+let list2 = readLine().split(" ");
+let list3 = readLine().split(" ") ;  
+ConvertToNumber(list1);
+ConvertToNumber(list2);
+ConvertToNumber(list3);
+let m1 = minimumValue(list1);
+let m2 = maximumValue(list2);
+let minMaxRange = getNumbersInRange(list3, m1, m2);
+console.log(...minMaxRange);
