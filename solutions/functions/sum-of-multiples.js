d9b081b53121b403a614b08c25327a8a124e241a
Abduliitr <abdul.thomso@gmail.com>
2022-03-09 11:11:58 +0530

    Updated as per latest template & comments addressed

--- solutions/functions/sum-of-multiples.js
+++ solutions/functions/sum-of-multiples.js
@@ -12,12 +12,12 @@ function readLine() {
 
 //Write  your function here
 
-function sum_of_multiples(list, multiple){
+function sumOfMultiples(list, divisor){
 
     let result = 0;
     
     for(let i = 0; i < list.length; i++){
-        if(list[i] % multiple === 0){
+        if(list[i] % divisor === 0){
             result += list[i];
         }
     }
@@ -27,10 +27,10 @@ function sum_of_multiples(list, multiple){
 
 
 // DO NOT CHANGE ANYTHING BELOW THIS LINE
-var range =parseInt(readLine())
-var numbers=[]
+let range =parseInt(readLine());
+let numbers=[];
 for(let each=0;each<range;each++){
-  numbers.push(parseInt(readLine()))
+  numbers.push(parseInt(readLine()));
 }
-var mult=parseInt(readLine())
-console.log(sum_of_multiples(numbers, mult))
+let mult=parseInt(readLine());
+console.log(sumOfMultiples(numbers, mult));
