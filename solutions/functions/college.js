a61e111d4874702d15abb90098b6b907ca8412ef
Abduliitr <abdul.thomso@gmail.com>
2022-03-09 11:06:54 +0530

    Updated as per latest template - and addressed comments

--- solutions/functions/college.js
+++ solutions/functions/college.js
@@ -9,15 +9,18 @@ function readLine() {
 }
 
 // -------- Do NOT edit anything above this line ----------
-function sumofdivisors(n){
-    //Code here
+function sumOfDivisors(n){
+  //Code here
 
     let requiredSum = 0;
 
-    for(let i = 1; i <= (parseInt(Math.sqrt(n))+1); i++){
+    let endValue = Math.floor(Math.sqrt(n)) + 1;
+    for(let i = 1; i <= endValue; i++){
         if(n % i === 0){
             requiredSum += i;
 
+            // If i is a factor of n, then n/i is also a factor of n.
+            // This if condition is to make sure, we are not counting same factor twice.
             if(n / i !== i){
                 requiredSum += n / i;
             }
@@ -28,5 +31,5 @@ function sumofdivisors(n){
 }
     
 
-let n = parseInt(readLine())
-console.log(sumofdivisors(n))
+let n = parseInt(readLine());
+console.log(sumOfDivisors(n));
