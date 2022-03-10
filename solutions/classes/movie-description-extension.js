132f3963398f39927bf33f2da17c844a278ef613
P.N.V.SATYANARAYANA <77286948+satya-9@users.noreply.github.com>
2022-03-10 17:31:19 +0530

    Update movie-description-extension.js

--- solutions/classes/movie-description-extension.js
+++ solutions/classes/movie-description-extension.js
@@ -9,18 +9,18 @@ function readLine() {
 }
 
 // -------- Do NOT edit anything above this line ----------
-const Movie = class {
+class Movie{
   constructor(
-    length_in_minutes = 120,
-    num_characters = 4,
+    lengthInminutes = 120,
+    numCharacters = 4,
     language = "English"
   ) {
-    this.length_in_minutes = length_in_minutes;
-    this.num_characters = num_characters;
+    this.lengthInminutes = lengthInminutes;
+    this.numCharacters = numCharacters;
     this.language = language;
   }
   certify() {
-    if (this.num_characters >= 2 && this.length_in_minutes <= 240) {
+    if (this.numCharacters >= 2 && this.lengthInminutes <= 240) {
       return true;
     } else {
       return false;
@@ -33,15 +33,15 @@ const Movie = class {
     }
     let text = "This is a ";
     text = text + this.language + " movie with ";
-    text = text + this.num_characters + " characters, is ";
-    text = text + this.length_in_minutes + " minutes long, and is ";
+    text = text + this.numCharacters + " characters, is ";
+    text = text + this.lengthInminutes + " minutes long, and is ";
     text = text + flag + "certified.";
     return text;
   }
 };
 
 var language = readLine();
-var num_characters = readLine();
-var length_in_minutes = readLine();
+var numCharacters = readLine();
+var lengthInminutes = readLine();
 
-console.log(new Movie(length_in_minutes, num_characters, language).run());
+console.log(new Movie(lengthInminutes, numCharacters, language).run());
