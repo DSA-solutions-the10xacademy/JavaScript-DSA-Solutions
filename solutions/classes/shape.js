406beef7920f9786bb39c775b64c0e6df4ac0c6a
Rahulkug <49363658+Rahulkug@users.noreply.github.com>
2022-03-10 19:35:22 +0530

    Classes - shape (#102)
    * shape
    
    * updated the template code and  class declaration
    
    Co-authored-by: LVSsandeepVarma <88339626+LVSsandeepVarma@users.noreply.github.com>

---
+++ solutions/classes/shape.js
@@ -0,0 +1,30 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+  idx++;
+  return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+
+// Define the required class here...
+
+class Rectangle {
+  constructor(length, width) {
+    this.length = length;
+    this.width = width;
+  }
+  calculatePerimeter() {
+    return 2 * (this.length + this.width);
+  }
+};
+
+// DO NOT CHANGE ANYTHING BELOW THIS LINE
+let input = readLine().split(" ");
+let length = parseInt(input[0]);
+let width = parseInt(input[1]); 
+let rectangle = new Rectangle(length, width);
+console.log(rectangle.calculatePerimeter());
