d277b8a314e0df4244d565c15dc471b28f13fe33
Rahulkug <49363658+Rahulkug@users.noreply.github.com>
2022-03-15 08:43:14 +0530

    Classes - movie description extension (#97)
    * movie-description-extension
    
    * Update movie-description-extension.js
    
    * Update movie-description-extension.js
    
    * Update movie-description-extension.js
    
    * Update movie-description-extension.js
    
    * Update movie-description-extension.js
    
    * Update movie-description-extension.js
    
    Co-authored-by: P.N.V.SATYANARAYANA <77286948+satya-9@users.noreply.github.com>

---
+++ solutions/classes/movie-description-extension.js
@@ -0,0 +1,44 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, "utf-8");
+let idx = 0;
+data = data.split("\n");
+
+function readLine() {
+  idx++;
+  return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+class Movie {
+  constructor(lengthInMinutes, numCharacters, language) {
+    this.lengthInMinutes = lengthInMinutes;
+    this.numCharacters = numCharacters;
+    this.language = language;
+  }
+  certify() {
+    if (this.numCharacters >= 2 && this.lengthInMinutes <= 240) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  run() {
+    let flag = "not ";
+    if (this.certify() === true) {
+      flag = "";
+    }
+    let text = "This is a ";
+    text = text + this.language + " movie with ";
+    text = text + this.numCharacters + " characters, is ";
+    text = text + this.lengthInMinutes + " minutes long, and is ";
+    text = text + flag + "certified.";
+    return text;
+  }
+};
+
+let language = readLine();
+let numCharacters = parseInt(readLine());
+let lengthInMinutes = parseInt(readLine());
+
+let movie =  new Movie(lengthInMinutes, numCharacters, language)
+console.log(movie.run());
