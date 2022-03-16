c0c9b71a61818a8dd0d21c613f17b92a8da30613
LVSsandeepVarma <88339626+LVSsandeepVarma@users.noreply.github.com>
2022-03-16 17:58:32 +0530

    Update reduce-array.js
    added semicolons after each statement and modified the code

--- solutions/arrays/reduce-array.js
+++ solutions/arrays/reduce-array.js
@@ -12,28 +12,21 @@ function readLine() {
 // Use readLine() for taking input, it will read one line of from the input  and is stored in string format
 let arraySize=parseInt(readLine());
 let arr=[];
-for(let index=0;index<arraySize;index++)
-{
+for(let index=0;index<arraySize;index++){
 	arr.push(parseInt(readLine()));
 }
-if(arraySize===1)
-{
+if(arraySize===1){
 	console.log(arr[0]);
 }
-else
-{
+else{
 	let answer=arr[0];
-	for(let index=1;index<arraySize;index++)
-	{
-		if(index%2===0)
-		{
+	for(let index=1;index<arraySize;index++){
+		if(index%2===0){
 			answer=answer-arr[index];
 		}
-		else if(index%2===1)
-		{
+		else if(index%2===1){
 			answer=answer+arr[index];
 		}
-		
 	}
 	console.log(answer);
 }
