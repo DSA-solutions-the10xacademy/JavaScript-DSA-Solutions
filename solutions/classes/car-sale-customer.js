804a673f1559e9d5bf3d720f9deae914fb5733e9
Rahulkug <49363658+Rahulkug@users.noreply.github.com>
2022-03-17 10:18:24 +0530

    Classes - car sale customer (#91)
    * car-sale-customer
    
    * Updated template, variables & class declaration
    
    * changes to the flag, count logic
    
    * changes to the flag, count logic
    
    * Comment when there are no promising customers
    
    Co-authored-by: rpapavan97 <kandivalasapavankumar@gmail.com>
    Co-authored-by: Sailesh Ramayanam <33143312+sailesh-ramayanam@users.noreply.github.com>

---
+++ solutions/classes/car-sale-customer.js
@@ -0,0 +1,44 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, "utf-8");
+let idx = 0;
+data = data.split("\n");
+
+function readLine() {
+  idx++;
+  return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+
+// Your class should be named `CarSell`.
+// Your method should be named `getPromisingCustomers`
+// Your method should return the indices of customers who are willing to pay greater than or equal to 90% of the car value
+class CarSell {
+  constructor(customerProposals) {
+    this.customerProposals = customerProposals;
+  }
+  getPromisingCustomers() {
+    let promisingcustomers = [];
+    for (let i=0;i<this.customerProposals.length;i++) {
+      if (this.customerProposals[i] >= 900000) {
+        promisingcustomers.push(i);
+      }
+    }
+    if (promisingcustomers.length===0) {
+      // If everyone is offering less than 90%, then we have to output -1
+      promisingcustomers.push(-1);
+    }
+    return promisingcustomers;
+  }
+};
+
+let numCustomers = parseInt(readLine());
+let customerProposals = [];
+for (let i = 0; i < numCustomers; i++) {
+  customerProposals.push(parseInt(readLine()));
+}
+
+let car = new CarSell(customerProposals);
+for (let j of car.getPromisingCustomers()) {
+  console.log(j);
+}
