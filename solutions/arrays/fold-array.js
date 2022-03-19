e48d5a36bad251485d10fe1e2edf93dd017462bc
DevratAgarwal <81330019+DevratAgarwal@users.noreply.github.com>
2022-03-19 18:09:49 +0530

    Arrays - fold array

--- solutions/arrays/fold-array.js
+++ solutions/arrays/fold-array.js
@@ -10,30 +10,35 @@ function readLine() {
 
 // -------- Do NOT edit anything above this line ----------
 // Use readLine() for taking input, it will read one line of from the input  and is stored in string format
-let arraySize = parseInt(readLine());
-let arr = [];
-for(let index = 0; index < arraySize; index++){
+let arraySize=parseInt(readLine());
+let arr=[];
+for(let index=0;index<arraySize;index++)
+{
 	arr.push(parseInt(readLine()));
 }
-let numberOfFolds = parseInt(readLine());
-
-for(let index = 0; index < numberOfFolds; index++){
-	let temporaryArray = [];
-	let leftIndex = 0, rightIndex = arr.length-1;
-	while(leftIndex <= rightIndex){
-		if(leftIndex != rightIndex){
-			temporaryArray.push(arr[leftIndex] + arr[rightIndex]);
+let numberOfFolds=parseInt(readLine());
+for(let index=0;index<numberOfFolds;index++)
+{
+	let temporaryArray=[];
+	let leftIndex=0;
+	let rightIndex=arr.length-1;
+	while(leftIndex <= rightIndex)
+	{
+		if(leftIndex != rightIndex)
+		{
+			temporaryArray.push(arr[leftIndex]+arr[rightIndex]);
 		}
-		else{
+		else
+		{
 			temporaryArray.push(arr[leftIndex]);
 		}
-		leftIndex = leftIndex + 1;
-		rightIndex = rightIndex - 1;
+		leftIndex = leftIndex+1;
+		rightIndex = rightIndex-1;
 	}
-	arr = temporaryArray;
+	arr=temporaryArray;
 }
-
 console.log(arr.length);
-for(let index = 0; index < arr.length; index++){
+for(let index=0;index<arr.length;index++)
+{
 	console.log(arr[index]);
 }
