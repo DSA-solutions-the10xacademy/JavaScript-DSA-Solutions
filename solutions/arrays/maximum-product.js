b45c558222db2f9d26a20716a576ea6950bec4e3
DevratAgarwal <81330019+DevratAgarwal@users.noreply.github.com>
2022-03-20 12:37:38 +0530

    Update maximum-product.js
    changes addressed

--- solutions/arrays/maximum-product.js
+++ solutions/arrays/maximum-product.js
@@ -12,11 +12,11 @@ function readLine() {
 // Use readLine() for taking input, it will read one line of from the input  and is stored in string format
 let arrraySize = parseInt(readLine());
 let firstNumber=parseInt(readLine());
-let secondNumber;
+
 let answer=-99999;
 for(let index=0;index<arrraySize-1;index++)
 {
-	secondNumber=parseInt(readLine());
+	let  secondNumber=parseInt(readLine());
   answer=Math.max(answer,firstNumber*secondNumber);
   firstNumber=secondNumber;
 }
