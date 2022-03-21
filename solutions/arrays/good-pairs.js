209af6750a4ae21422466869fb3c346d191e9f85
DevratAgarwal <81330019+DevratAgarwal@users.noreply.github.com>
2022-03-21 19:52:32 +0530

    Array - good pairs (#283)
    * Array - good pairs
    
    * Rename good-pairs.js to solutions/arrays/good-pairs.js
    
    * Add files via upload
    
    * Formatted the code
    
    Co-authored-by: LVSsandeepVarma <88339626+LVSsandeepVarma@users.noreply.github.com>
    Co-authored-by: Sailesh Ramayanam <33143312+sailesh-ramayanam@users.noreply.github.com>

---
+++ solutions/arrays/good-pairs.js
@@ -0,0 +1,25 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+  idx++;
+  return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
+let arr = readLine().split(" ");
+for (let index = 0; index < arr.length; index++) {
+    arr[index] = parseInt(arr[index]);
+}
+let count = 0;
+for (let focusedIndex = 0; focusedIndex < arr.length; focusedIndex++) {
+    for (let comparedIndex = focusedIndex + 1; comparedIndex < arr.length; comparedIndex++) {
+        if (arr[focusedIndex] === arr[comparedIndex]) {
+            count = count + 1;
+        }
+    }
+}
+console.log(count);
