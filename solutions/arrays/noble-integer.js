df2dbda32f4377beaadedc5f10675f8df5a91947
Sailesh Ramayanam <33143312+sailesh-ramayanam@users.noreply.github.com>
2022-03-21 14:51:28 +0530

    Formatted and added comments

--- solutions/arrays/noble-integer.js
+++ solutions/arrays/noble-integer.js
@@ -10,27 +10,25 @@ function readLine() {
 
 // -------- Do NOT edit anything above this line ----------
 // Use readLine() for taking input, it will read one line of from the input  and is stored in string format
-let arraySize=parseInt(readLine());
-let arr=[];
-for(let index=0;index<arraySize;index++)
-{
-	arr.push(parseInt(readLine()));
+let arraySize = parseInt(readLine());
+let arr = [];
+for (let index = 0; index < arraySize; index++) {
+    arr.push(parseInt(readLine()));
 }
-let answer=-1;
-for(let focusedIndex=0;focusedIndex<arraySize;focusedIndex++)
-{
-	let count=0;
-	for(let currIndex=0;currIndex<arraySize;currIndex++)
-	{
-		if(arr[currIndex]>arr[focusedIndex])
-		{
-			count=count+1;
-		}
-	}
-	if(count===arr[focusedIndex])
-		{
-			answer=1;
-			break;
-		}
+// Assume that the given array does not contain any noble integer
+let answer = -1;
+for (let focusedIndex = 0; focusedIndex < arraySize; focusedIndex++) {
+    let count = 0;
+    for (let currIndex = 0; currIndex < arraySize; currIndex++) {
+        if (arr[currIndex] > arr[focusedIndex]) {
+            count = count + 1;
+        }
+    }
+    if (count === arr[focusedIndex]) {
+        // We have found a noble integer. Update the answer.
+        // Also, we don't need to search further.
+        answer = 1;
+        break;
+    }
 }
 console.log(answer);
