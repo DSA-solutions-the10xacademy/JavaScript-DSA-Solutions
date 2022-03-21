4cdceb653418bc5d01af45d57f8abedf697a364a
DevratAgarwal <81330019+DevratAgarwal@users.noreply.github.com>
2022-03-21 23:16:22 +0530

    Update rotate-matrix.js

--- solutions/arrays/rotate-matrix.js
+++ solutions/arrays/rotate-matrix.js
@@ -4,50 +4,31 @@ let idx = 0;
 data = data.split('\n');
 
 function readLine() {
-  idx++;
-  return data[idx - 1];
+	idx++;
+	return data[idx - 1];
 }
 
 // -------- Do NOT edit anything above this line ----------
 // Use readLine() for taking input, it will read one line of from the input  and is stored in string format
-function rotate(lst)
-{
-	
-	let answer=[];
-	for(let column=0;column<lst[0].length;column++)
-	{
-		let temporaryArray=[];
-		for(let row=0;row<lst.length;row++)
-		{
-			temporaryArray.push(lst[row][column])
-		}
-		answer.push(temporaryArray);
+
+let numRows = parseInt(readLine());
+let matrix = [];
+for (let rowIndex = 0; rowIndex < numRows; rowIndex++) {
+	let currRow = readLine().split(" ");
+	for (let index = 0; index < currRow.length; index++) {
+		currRow[index] = parseInt(currRow[index]);
 	}
-    for(let row=0;row<answer.length;row++)
-    {
-        for(let column=0;column<answer[0].length/2;column++)
-        {
-            let temporary = answer[row][column];
-            answer[row][column]=answer[row][answer[0].length-1-column];
-            answer[row][answer[0].length-1-column]=temporary;
-        }
-    }
-	return answer;
+	matrix.push(currRow);
 }
-let val = parseInt(readLine());
-let lst = [];
-for(let row=0;row<val;row++)
-{
-	let temporaryArray=readLine().split(" ");
-	for(let index=0;index<temporaryArray.length;index++)
-	{
-		temporaryArray[index]=parseInt(temporaryArray[index]);
+console.log(matrix[0].length);
+let rotatedMatrix = [];
+for (let column = 0; column < matrix[0].length; column++) {
+	let newRow = [];
+	for (let row = matrix.length - 1; row >= 0; row--) {
+		newRow.push(matrix[row][column])
 	}
-	lst.push(temporaryArray);
-    
+	rotatedMatrix.push(newRow);
 }
-console.log(lst[0].length);
-let out=rotate(lst)
-for(val of out){
-	console.log(...val);
+for (row of rotatedMatrix) {
+	console.log(...row);
 }
