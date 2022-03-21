edbe33a1f80fd5bfcac19637465904863f8d2f4e
DevratAgarwal <81330019+DevratAgarwal@users.noreply.github.com>
2022-03-21 22:58:51 +0530

    Update reduce-array.js

--- solutions/arrays/reduce-array.js
+++ solutions/arrays/reduce-array.js
@@ -4,32 +4,20 @@ let idx = 0;
 data = data.split('\n');
 
 function readLine() {
-  idx++;
-  return data[idx - 1];
+	idx++;
+	return data[idx - 1];
 }
 
 // -------- Do NOT edit anything above this line ----------
 // Use readLine() for taking input, it will read one line of from the input  and is stored in string format
-let n=parseInt(readLine())
-let answer=parseInt(readLine());
-if(arraySize===1)
-{
-	console.log(answer);
-}
-else
-{
-	answer=answer+parseInt(readLine());
-	for(let index=2;index<nindex++)
-	{
-		if(index%2===0)
-		{
-			answer=answer-parseInt(readLine());
-		}
-		else if(index%2===1)
-		{
-			answer=answer+parseInt(readLine());
-		}
-		
+let arraySize = parseInt(readLine())
+let answer = parseInt(readLine());
+for (let index = 1; index < arraySize; index++) {
+	if (index % 2 === 0) {
+		answer = answer - parseInt(readLine());
 	}
-	console.log(answer);
+	else if (index % 2 === 1) {
+		answer = answer + parseInt(readLine());
+	}
 }
+console.log(answer);
