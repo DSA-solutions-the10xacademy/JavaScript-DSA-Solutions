0fb9b1d6f2773ffbbfdfb891c655fd54055a1c3b
Sailesh Ramayanam <33143312+sailesh-ramayanam@users.noreply.github.com>
2022-03-21 19:56:39 +0530

    Formatted the code

--- solutions/arrays/max-product.js
+++ solutions/arrays/max-product.js
@@ -10,27 +10,23 @@ function readLine() {
 
 // -------- Do NOT edit anything above this line ----------
 // Use readLine() for taking input, it will read one line of from the input  and is stored in string format
-let arrayOneSize=parseInt(readLine());
-let arrayTwoSize=parseInt(readLine());
-let arr1=readLine().split(" ");
-let arr2=readLine().split(" ");
-for(let index=0;index<arrayOneSize;index++)
-{
-	arr1[index]=parseInt(arr1[index]);
+let arrayOneSize = parseInt(readLine());
+let arrayTwoSize = parseInt(readLine());
+let arr1 = readLine().split(" ");
+let arr2 = readLine().split(" ");
+for (let index = 0; index < arrayOneSize; index++) {
+    arr1[index] = parseInt(arr1[index]);
 }
-for(let index=0;index<arrayTwoSize;index++)
-{
-	arr2[index]=parseInt(arr2[index]);
+for (let index = 0; index < arrayTwoSize; index++) {
+    arr2[index] = parseInt(arr2[index]);
 }
-let maximumArrayOne=Math.abs(arr1[0]);
-let maximumArrayTwo=Math.abs(arr2[0]);
-for(let index=0;index<arrayOneSize;index++)
-{
+let maximumArrayOne = Math.abs(arr1[0]);
+let maximumArrayTwo = Math.abs(arr2[0]);
+for (let index = 0; index < arrayOneSize; index++) {
 
-	maximumArrayOne=Math.max(maximumArrayOne,Math.abs(arr1[index]));
+    maximumArrayOne = Math.max(maximumArrayOne, Math.abs(arr1[index]));
 }
-for(let index=0;index<arrayTwoSize;index++)
-{
-	maximumArrayTwo=Math.max(maximumArrayTwo,Math.abs(arr2[index]));
+for (let index = 0; index < arrayTwoSize; index++) {
+    maximumArrayTwo = Math.max(maximumArrayTwo, Math.abs(arr2[index]));
 }
-console.log(maximumArrayTwo*maximumArrayOne);
+console.log(maximumArrayTwo * maximumArrayOne);
