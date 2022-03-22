01a40b5971c339b8cd4e5c6a54fb4dfd8556454c
Sailesh Ramayanam <33143312+sailesh-ramayanam@users.noreply.github.com>
2022-03-22 11:58:40 +0530

    Changed logic from number based to index based

--- solutions/arrays/pascal-triangle.js
+++ solutions/arrays/pascal-triangle.js
@@ -11,33 +11,28 @@ function readLine() {
 // -------- Do NOT edit anything above this line ----------
 // Use readLine() for taking input, it will read one line of from the input  and is stored in string format
 let N = parseInt(readLine());
-if(N ===1)
-{
-	console.log(1);
+if (N === 1) {
+    console.log(1);
 }
-else
-{
-	let prevRow=[1];
-	for(let row=2;row<=N;row++)
-	{
-		let currRow=[];
-		for(let column=1;column<=row;column++)
-		{
-			if(column===1)
-			{
-				currRow.push(prevRow[0]);
-			}
-			else if(column===row)
-			{
-				currRow.push(prevRow[prevRow.length-1]);
-			}
-			else
-			{
-				currRow.push(prevRow[column-1]+prevRow[column-2]);
-			}
-		}
-		prevRow=currRow
-	}
-	for(let column=1;column<=N;column++)
-	console.log(prevRow[column-1])
+else {
+    let prevRow = [1];
+    for (let rowIndex = 1; rowIndex < N; rowIndex++) {
+        let numColumns = rowIndex + 1; // 0th row has 1 element, 1st row has 2 elements and so on
+        let currRow = [];
+        for (let colIndex = 0; colIndex < numColumns; colIndex++) {
+            if (colIndex === 0) {
+                currRow.push(prevRow[0]);
+            }
+            else if (colIndex === rowIndex) {
+                currRow.push(prevRow[prevRow.length - 1]);
+            }
+            else {
+                currRow.push(prevRow[colIndex] + prevRow[colIndex - 1]);
+            }
+        }
+        prevRow = currRow;
+    }
+    for (let colIndex = 0; colIndex < N; colIndex++) {
+        console.log(prevRow[colIndex]);
+    }
 }
