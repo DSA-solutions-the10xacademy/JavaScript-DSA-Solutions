ce844aa1ecc8aa6e8a122dc920e52860aa996d8f
Sailesh Ramayanam <33143312+sailesh-ramayanam@users.noreply.github.com>
2022-03-22 15:05:01 +0530

    Added formatting

--- solutions/arrays/shuffle-array.js
+++ solutions/arrays/shuffle-array.js
@@ -10,28 +10,27 @@ function readLine() {
 
 // -------- Do NOT edit anything above this line ----------
 
-function shuffle(arr){
+function shuffle(arr) {
     //Implement this function
-	let leftIndex=0;
-	let rightIndex=arr.length/2;
-	let answer=[];
-	while(leftIndex < arr.length/2)
-	{
-		answer.push(arr[leftIndex]);
-		answer.push(arr[rightIndex]);
-		leftIndex = leftIndex+1;
-		rightIndex = rightIndex+1;
-	}
-	return answer;
+    let leftIndex = 0;
+    let rightIndex = arr.length / 2;
+    let answer = [];
+    while (leftIndex < arr.length / 2) {
+        answer.push(arr[leftIndex]);
+        answer.push(arr[rightIndex]);
+        leftIndex = leftIndex + 1;
+        rightIndex = rightIndex + 1;
+    }
+    return answer;
 }
 
 // Do not edit anything below
-let n = parseInt(readLine())
-let nums = []
-for(let index=0;index<2*n;index++){
-        nums.push(parseInt(readLine()))
+let n = parseInt(readLine());
+let nums = [];
+for (let index = 0; index < 2 * n; index++) {
+    nums.push(parseInt(readLine()));
 }
-let res=shuffle(nums)
-for(elem of res){
-  console.log(elem)
+let res = shuffle(nums);
+for (elem of res) {
+    console.log(elem);
 }
