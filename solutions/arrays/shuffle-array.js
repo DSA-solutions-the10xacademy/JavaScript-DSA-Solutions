8ea57411eb6be0a5530c55d20f7871de9869016c
DevratAgarwal <81330019+DevratAgarwal@users.noreply.github.com>
2022-03-22 16:02:26 +0530

    Array - shuffle array (#278)
    * Array - shuffle array
    
    * Rename shuffle-array.js to solutions/arrays/shuffle-array.js
    
    * Add files via upload
    
    * Added formatting
    
    Co-authored-by: LVSsandeepVarma <88339626+LVSsandeepVarma@users.noreply.github.com>
    Co-authored-by: Sailesh Ramayanam <33143312+sailesh-ramayanam@users.noreply.github.com>

---
+++ solutions/arrays/shuffle-array.js
@@ -0,0 +1,36 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+  idx++;
+  return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+
+function shuffle(arr) {
+    //Implement this function
+    let leftIndex = 0;
+    let rightIndex = arr.length / 2;
+    let answer = [];
+    while (leftIndex < arr.length / 2) {
+        answer.push(arr[leftIndex]);
+        answer.push(arr[rightIndex]);
+        leftIndex = leftIndex + 1;
+        rightIndex = rightIndex + 1;
+    }
+    return answer;
+}
+
+// Do not edit anything below
+let n = parseInt(readLine());
+let nums = [];
+for (let index = 0; index < 2 * n; index++) {
+    nums.push(parseInt(readLine()));
+}
+let res = shuffle(nums);
+for (elem of res) {
+    console.log(elem);
+}
