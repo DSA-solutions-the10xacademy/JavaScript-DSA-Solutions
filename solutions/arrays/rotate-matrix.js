e4e17ae7f33ab7ae0033698bea2246efa68a02fb
DevratAgarwal <81330019+DevratAgarwal@users.noreply.github.com>
2022-03-22 08:16:46 +0530

    Array - rotate matrix (#284)
    * Array - rotate matrix
    
    * Rename rotate-matrix.js to arrays/rotate-matrix.js
    
    * Rename arrays/rotate-matrix.js to solutions/arrays/rotate-matrix.js
    
    * Updated logic & coding conventions
    
    * Arrays - rotate matrix
    
    * Update rotate-matrix.js
    
    * Refactored all output to one place
    
    Co-authored-by: LVSsandeepVarma <88339626+LVSsandeepVarma@users.noreply.github.com>
    Co-authored-by: rpapavan97 <kandivalasapavankumar@gmail.com>
    Co-authored-by: Sailesh Ramayanam <33143312+sailesh-ramayanam@users.noreply.github.com>

---
+++ solutions/arrays/rotate-matrix.js
@@ -0,0 +1,36 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+	idx++;
+	return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
+
+let numRows = parseInt(readLine());
+let matrix = [];
+for (let rowIndex = 0; rowIndex < numRows; rowIndex++) {
+	let currRow = readLine().split(" ");
+	for (let index = 0; index < currRow.length; index++) {
+		currRow[index] = parseInt(currRow[index]);
+	}
+	matrix.push(currRow);
+}
+
+let rotatedMatrix = [];
+for (let column = 0; column < matrix[0].length; column++) {
+	let newRow = [];
+	for (let row = matrix.length - 1; row >= 0; row--) {
+		newRow.push(matrix[row][column]);
+	}
+	rotatedMatrix.push(newRow);
+}
+
+console.log(rotatedMatrix.length);
+for (row of rotatedMatrix) {
+	console.log(...row);
+}
