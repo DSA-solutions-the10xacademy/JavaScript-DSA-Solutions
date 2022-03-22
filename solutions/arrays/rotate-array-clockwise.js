019c44a8ee6ca23cfb6b05d2de9e60f35ef79b18
DevratAgarwal <81330019+DevratAgarwal@users.noreply.github.com>
2022-03-22 16:29:26 +0530

    Update rotate-array-clockwise.js

--- solutions/arrays/rotate-array-clockwise.js
+++ solutions/arrays/rotate-array-clockwise.js
@@ -4,21 +4,21 @@ let idx = 0;
 data = data.split('\n');
 
 function readLine() {
-  idx++;
-  return data[idx - 1];
+	idx++;
+	return data[idx - 1];
 }
 
 // -------- Do NOT edit anything above this line ----------
 // Use readLine() for taking input, it will read one line of from the input  and is stored in string format
 let arr = readLine().split(" ");
 let rotate = parseInt(readLine());
-
-while(rotate > 0){
-	let shifted = arr.shift();
-	arr.push(shifted);
-	rotate--;
+let answer = [];
+for (let index = rotate; index < arr.length; index++) {
+	answer.push(arr[index]);
 }
-
-for(let index = 0; index < arr.length; index++){
-	console.log(arr[index]);
+for (let index = 0; index < rotate; index++) {
+	answer.push(arr[index]);
+}
+for (let index = 0; index < arr.length; index++) {
+	console.log(answer[index]);
 }
