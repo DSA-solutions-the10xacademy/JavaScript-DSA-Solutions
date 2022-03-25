52381a38f4cc0fd496adfd2f2c46894afa7c68dc
Abdulahad Khan <43092512+Abduliitr@users.noreply.github.com>
2022-03-25 15:08:32 +0530

    Arrays - Sum Of Diagonals (#303)
    * Arrays - Sum Of Diagonals
    
    * Updated as per review comments

---
+++ solutions/arrays/sum-of-diagonals.js
@@ -0,0 +1,39 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+  idx++;
+  return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
+
+function convertToNumber(list){
+    for(let i = 0; i < list.length; i++){
+        list[i] = parseInt(list[i]);
+    }
+
+    return list;
+}
+
+let rows = parseInt(readLine());
+
+let matrix = [];
+for(let i = 0; i < rows; i++){
+    let eachRow = readLine().split(" ");
+    eachRow = convertToNumber(eachRow);
+    matrix.push(eachRow);
+}
+
+let sumFirstDiagonal = 0;
+let sumSecondDiagonal = 0;
+
+for(let i = 0; i < matrix.length; i++){
+    sumFirstDiagonal += matrix[i][i];
+    sumSecondDiagonal += matrix[i][matrix.length - 1 - i];
+}
+
+console.log(sumFirstDiagonal + sumSecondDiagonal);
