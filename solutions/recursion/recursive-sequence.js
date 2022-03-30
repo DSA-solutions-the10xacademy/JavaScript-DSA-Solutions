cee568f0303aaa98d5dcab7541c02102a505cf0d
Prateek Mehra <partumehra@gmail.com>
2022-03-30 17:37:47 +0530

    changed iterative solution to recursive

--- solutions/recursion/recursive-sequence.js
+++ solutions/recursion/recursive-sequence.js
@@ -11,26 +11,26 @@ function readLine() {
 // -------- Do NOT edit anything above this line ----------
 // Use readLine() for taking input, it will read one line of from the input  and is stored in string format
 
+function f(operands, num, n){
+
+    if(operands == n+1)
+        return 0;
+
+    let currentTerm = 1;
+
+    for(let i = 1; i <= operands; i++){
+        currentTerm *= num;
+        num++;
+    }
+
+    return currentTerm + f(operands+1, num, n);
+}
+
 let testCases = parseInt(readLine());
 
 for(let i = 0; i < testCases; i++){
     
     let n = parseInt(readLine());
-    let nthTerm = 0;
-    let num = 1;
-
-    for(let j = 1; j <= n; j++){
-        
-        let operands = 0;
-        let currentTerm = 1;
-        while(operands < j){
-            operands++;
-            currentTerm *= num;
-            num++;
-        }
-
-        nthTerm += currentTerm;
-    }
-
-    console.log(nthTerm);
+    
+    console.log(f(1,1,n));
 }
