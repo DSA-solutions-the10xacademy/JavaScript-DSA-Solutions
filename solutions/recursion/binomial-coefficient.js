3f1dfe11220548df6f05b09892f3c8869ff52845
DevratAgarwal <81330019+DevratAgarwal@users.noreply.github.com>
2022-03-30 11:41:23 +0530

    Create binomial-coefficient.js

---
+++ solutions/recursion/binomial-coefficient.js
@@ -0,0 +1,35 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+    idx++;
+    return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+
+
+
+function binomialCoeff(n, r) {
+    //Implement this function
+    if (r > n) {
+        return 0;
+    }
+    else if (r === 0 || n === r) {
+        return 1;
+    }
+    else {
+        return binomialCoeff(n - 1, r - 1) + binomialCoeff(n - 1, r);
+    }
+
+}
+
+let testCases = parseInt(readLine());
+for (let index = 0; index < testCases; index++) {
+    let input = readLine().split(" ");
+    n = parseInt(input[0]);
+    let r = parseInt(input[1]);
+    console.log(binomialCoeff(n, r));
+}
