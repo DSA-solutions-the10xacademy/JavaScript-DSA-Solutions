77de244213f6d92eea2afbd24f481bef618e8952
shivateja1729 <95900805+shivateja1729@users.noreply.github.com>
2022-04-01 15:11:04 +0530

    Update add-substract.js

--- solutions/recursion/add-substract.js
+++ solutions/recursion/add-substract.js
@@ -4,22 +4,29 @@ let idx = 0;
 data = data.split('\n');
 
 function readLine() {
-    idx++;
-    return data[idx - 1];
+	idx++;
+	return data[idx - 1];
 }
-function NumberOfWays(array, sum, index) {
+
+// -------- Do NOT edit anything above this line ----------
+// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
+function numberOfWays(array, sum, index) {
     let arraySize = array.length;
     if (index === arraySize) {
-        if (sum === 0) { return 1; }
+        if (sum === 0) { 
+			return 1; 
+		}
         else {
             return 0;
         }
     }
-    return NumberOfWays(array, sum, index + 1) + NumberOfWays(array, sum - array[index], index + 1) + NumberOfWays(array, sum + array[index], index + 1);
+    return numberOfWays(array, sum, index + 1) + numberOfWays(array, sum - array[index], index + 1) + numberOfWays(array, sum + array[index], index + 1);
 }
-// -------- Do NOT edit anything above this line ----------
-// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
+// -------- Do NOT edit anything below this line ----------
 let sum = parseInt(readLine());
 let arraySize = parseInt(readLine());
-let array = readLine().split(" ").map(Number);
-console.log(NumberOfWays(array, sum, 0))
+let array = readLine().split(" ");
+for (let i = 0; i < arraySize; i++){
+	array[i] = parseInt(array[i]);
+}
+console.log(numberOfWays(array, sum, 0))
