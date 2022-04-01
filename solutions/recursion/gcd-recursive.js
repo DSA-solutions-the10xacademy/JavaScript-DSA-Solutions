1745799ceedc34ffda468b99eff046afd57fb4ab
shivateja1729 <95900805+shivateja1729@users.noreply.github.com>
2022-04-01 14:08:23 +0530

    Update gcd-recursive.js

--- solutions/recursion/gcd-recursive.js
+++ solutions/recursion/gcd-recursive.js
@@ -10,19 +10,20 @@ function readLine() {
 
 // -------- Do NOT edit anything above this line ----------
 
-function Gcd(divident, divisor) {
+function gcd(dividend, divisor) {
   // Implement this function
   if (divisor == 0) {
-    return divident;
+    return dividend;
   }
-  return Gcd(divisor, divident % divisor);
+  return gcd(divisor, dividend % divisor);
 }
 
 let testCases = parseInt(readLine());
 while (testCases > 0) {
   let input = readLine().split(" ");
-  let divident = parseInt(input[0]);
+  let dividend = parseInt(input[0]);
   let divisor = parseInt(input[1]);
-  console.log(Gcd(divident, divisor));
+  console.log(gcd(dividend, divisor));
   testCases--;
 }
+
