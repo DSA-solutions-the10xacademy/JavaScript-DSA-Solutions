e159316e1d790b197b3430ed419102b1778067b8
Devjyoti29 <devjyoti.rakshit@gmail.com>
2022-04-03 18:01:22 +0530

    revert

--- solutions/recursion/palindrome-recursive.js
+++ solutions/recursion/palindrome-recursive.js
@@ -1,28 +1,21 @@
-let fs = require("fs");
-let data = fs.readFileSync(0, "utf-8");
-let idx = 0;
-data = data.split("\n");
 
-function readLine() {
-  idx++;
-  return data[idx - 1];
+function isPalindrome(str,low,high)
+{
+	if(low>=high){
+		return true
+	}
+	else{
+		return str[low]==str[high] && isPalindome(str,low+1,high-1)
+	}
 }
-
-// -------- Do NOT edit anything above this line ----------
-// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
-function isPalindrome(str, low, high) {
-  if (low >= high) {
-    return true;
-  } else {
-    return str[low] == str[high] && isPalindrome(str, low + 1, high - 1);
-  }
-}
-let t = parseInt(readLine());
-for (let i = 0; i < t; i++) {
-  s = readLine();
-  if (isPalindrome(s, 0, s.length - 1)) {
-    console.log("True");
-  } else {
-    console.log("False");
-  }
+let t=1
+for(let i=0;i<t;i++)
+{
+	s="eye"
+	if (isPalindrome(s,0,s.length-1)){
+		console.log("True")
+	}
+	else{
+		console.log("False")
+	}
 }
