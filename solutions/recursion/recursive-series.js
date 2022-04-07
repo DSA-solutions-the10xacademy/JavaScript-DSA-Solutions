28f3515ff68347876c11a8c9d3573f9f5ed67180
Prateek Mehra <partumehra@gmail.com>
2022-04-07 21:06:17 +0530

    changed function name

--- solutions/recursion/recursive-series.js
+++ solutions/recursion/recursive-series.js
@@ -11,17 +11,17 @@ function readLine() {
 // -------- Do NOT edit anything above this line ----------
 // Use readLine() for taking input, it will read one line of from the input  and is stored in string format
 
-function s(n){
+function series(n){
 
     if(n <= 9){
         return n;
     }
 
     if(n % 2 == 0){
-        return s(n-10);
+        return series(n-10);
     }
 
-    return s(n-9);
+    return series(n-9);
 }
 
 let testCases = parseInt(readLine());
@@ -29,5 +29,5 @@ let testCases = parseInt(readLine());
 for(let i = 0; i < testCases; i++){
     let n = parseInt(readLine());
 
-    console.log(s(n));
+    console.log(series(n));
 }
