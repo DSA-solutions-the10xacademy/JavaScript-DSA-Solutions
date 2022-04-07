3de750778032d10214c3f68dbccdb51cc1dcbdb3
DevratAgarwal <81330019+DevratAgarwal@users.noreply.github.com>
2022-04-07 09:36:41 +0530

    Recursion - binomial coefficient (#341)
    * Create binomial-coefficient.js
    
    * Update binomial-coefficient.js
    
    * Minor change to template
    
    Co-authored-by: Sailesh Ramayanam <33143312+sailesh-ramayanam@users.noreply.github.com>

---
+++ solutions/recursion/binomial-coefficient.js
@@ -0,0 +1,34 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+    idx++;
+    return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+
+
+
+function binomialCoefficient(n, r) {
+    //Implement this function
+    if (r > n) {
+        return 0;
+    }
+    else if (r === 0 || n === r) {
+        return 1;
+    }
+    else {
+        return binomialCoefficient(n - 1, r - 1) + binomialCoefficient(n - 1, r);
+    }
+}
+
+let testCases = parseInt(readLine());
+for (let index = 0; index < testCases; index++) {
+    let input = readLine().split(" ");
+    let n = parseInt(input[0]);
+    let r = parseInt(input[1]);
+    console.log(binomialCoefficient(n, r));
+}
