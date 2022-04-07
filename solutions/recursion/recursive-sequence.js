9eb7260c8f2bf024a98d1d6e803637e30405f347
Prateek Mehra <partumehra@gmail.com>
2022-04-07 10:29:57 +0530

    modified the recursive approach and re-formatted code

--- solutions/recursion/recursive-sequence.js
+++ solutions/recursion/recursive-sequence.js
@@ -4,35 +4,28 @@ let idx = 0;
 data = data.split('\n');
 
 function readLine() {
-	idx++;
-	return data[idx - 1];
+    idx++;
+    return data[idx - 1];
 }
 
 // -------- Do NOT edit anything above this line ----------
 // Use readLine() for taking input, it will read one line of from the input  and is stored in string format
 
-function f(operands, num, n){
-
-    if(operands == n+1)
-        {
-            return 0;
-        }
-
-    let currentTerm = 1;
-
-    for(let i = 1; i <= operands; i++){
-        currentTerm *= num;
-        num++;
+function f(n) {
+    if(n == 1){
+        return 1;
     }
 
-    return currentTerm + f(operands+1, num, n);
+    let currentTerm = 1;
+    for (let i = n * (n - 1) / 2 + 1; i <= n * (n - 1) / 2 + n; i++) {
+        currentTerm *= i;
+    }
+    return currentTerm + f(n - 1);
 }
 
 let testCases = parseInt(readLine());
 
-for(let i = 0; i < testCases; i++){
-    
+for (let i = 0; i < testCases; i++) {
     let n = parseInt(readLine());
-    
-    console.log(f(1,1,n));
+    console.log(f(n));
 }
