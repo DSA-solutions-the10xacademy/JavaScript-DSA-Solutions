af132ad24fafb254746de69e2de447890b0eeb8e
Prateek Mehra <partumehra@gmail.com>
2022-04-14 21:12:35 +0530

    created intersection-sorted-array.js

---
+++ solutions/hashmaps/intersection-sorted-array.js
@@ -0,0 +1,69 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+    idx++;
+    return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
+
+let lengthOfArray1 = parseInt(readLine());
+
+let array1 = [];
+
+for (let i = 0; i < lengthOfArray1; i++) {
+    array1[i] = parseInt(readLine());
+}
+
+let lengthOfArray2 = parseInt(readLine());
+
+let array2 = [];
+
+for (let i = 0; i < lengthOfArray2; i++) {
+    array2[i] = parseInt(readLine());
+}
+
+const array1Frequency = new Map();
+
+for (let i = 0; i < lengthOfArray1; i++) {
+    if (array1Frequency.has(array1[i])) {
+        array1Frequency.set(array1[i], array1Frequency.get(array1[i]) + 1);
+    } else {
+        array1Frequency.set(array1[i], 1);
+    }
+}
+
+const array2Frequency = new Map();
+
+for (let i = 0; i < lengthOfArray2; i++) {
+    if (array2Frequency.has(array2[i])) {
+        array2Frequency.set(array2[i], array1Frequency.get(array2[i]) + 1);
+    } else {
+        array2Frequency.set(array2[i], 1);
+    }
+}
+
+let intersectionOfArrays = [];
+
+for (const key of array1Frequency.keys()) {
+    if (array2Frequency.has(key)) {
+        let numberOfCommonAppearances = Math.min(array1Frequency.get(key),array2Frequency.get(key));
+        while(numberOfCommonAppearances--){
+            intersectionOfArrays.push(key);
+        }
+    }
+}
+
+if (intersectionOfArrays.length == 0) {
+    console.log(-1);
+} else {
+    for (let i = 0; i < intersectionOfArrays.length; i++) {
+        console.log(intersectionOfArrays[i]);
+    }
+}
+
+
