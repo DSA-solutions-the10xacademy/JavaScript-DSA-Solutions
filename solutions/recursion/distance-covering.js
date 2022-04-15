a8ed05219e784c9d9b400de03cf318f5742f73e5
Sailesh Ramayanam <33143312+sailesh-ramayanam@users.noreply.github.com>
2022-04-15 12:20:08 +0530

    Refactoring for better readability

--- solutions/recursion/distance-covering.js
+++ solutions/recursion/distance-covering.js
@@ -10,17 +10,19 @@ function readLine() {
 
 // -------- Do NOT edit anything above this line ----------
 // Use readLine() for taking input, it will read one line of from the input  and is stored in string format
-function numberOFWays(targetPosition, currentPosition) {
-    if (targetPosition === currentPosition) {
+function numberOFWays(currentPosition, targetPosition) {
+    if (currentPosition === targetPosition) {
+        // We found one valid way.
         return 1;
     }
-    if (targetPosition < currentPosition) {
+    if (currentPosition > targetPosition) {
+        // We went beyond the target. So, this way is invalid.
         return 0;
     }
-    return numberOFWays(targetPosition, currentPosition + 1) + numberOFWays(targetPosition, currentPosition + 2);
+    return numberOFWays(currentPosition + 1, targetPosition) + numberOFWays(currentPosition + 2, targetPosition);
 }
-let testCases = parseInt(readLine());
-for (let testCase = 0; testCase < testCases; testCase++) {
+let numTests = parseInt(readLine());
+for (let testIndex = 0; testIndex < numTests; testIndex++) {
     let p = parseInt(readLine());
-    console.log(numberOFWays(p, 0));
+    console.log(numberOFWays(0, p));
 }
