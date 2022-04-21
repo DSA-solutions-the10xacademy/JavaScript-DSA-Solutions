ff899e9d66d8399dfe62a981beaba3bfafd6e51b
Prateek Mehra <partumehra@gmail.com>
2022-04-22 05:10:44 +0530

    created rating-contest.js

---
+++ solutions/control flow/rating-contest.js
@@ -0,0 +1,22 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+    idx++;
+    return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
+
+rating = parseInt(readLine());
+
+if (rating < 1200) {
+    console.log("ABC");
+} else if (rating < 2800) {
+    console.log("ARC");
+} else {
+    console.log("AGC");
+}
