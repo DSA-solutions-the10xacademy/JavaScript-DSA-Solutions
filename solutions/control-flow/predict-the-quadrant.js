882c21c77ad7d0a537ea418df1d59af69b70734d
shivateja1729 <95900805+shivateja1729@users.noreply.github.com>
2022-04-25 17:36:32 +0530

    Update predict-the-quadrant.js

--- solutions/control-flow/predict-the-quadrant.js
+++ solutions/control-flow/predict-the-quadrant.js
@@ -11,11 +11,11 @@ function readLine() {
 // -------- Do NOT edit anything above this line ----------
 // Use readLine() for taking input, it will read one line of from the input  and is stored in string format
 
-rating = parseInt(readLine());
+let rank = parseInt(readLine());
 
-if (rating < 1200) {
+if (rank < 1200) {
     console.log("ABC");
-} else if (rating < 2800) {
+} else if (rank < 2800) {
     console.log("ARC");
 } else {
     console.log("AGC");
