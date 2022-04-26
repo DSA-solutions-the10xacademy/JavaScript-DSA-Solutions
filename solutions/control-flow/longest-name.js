c41dfff284362d9bfc409d4fa267144eb2d10bf5
Abhishek Ranjan <abhishekranjan0505@gmail.com>
2022-04-27 04:59:53 +0530

    Control Flow - Longest name

---
+++ solutions/control-flow/longest-name.js
@@ -0,0 +1,28 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, "utf-8");
+let idx = 0;
+data = data.split("\n");
+
+function readLine() {
+  idx++;
+  return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
+
+let n = parseInt(readLine());
+let string_arr = readLine().split(" ");
+
+let max_length = 0;
+let res_string = "";
+
+for (let i = 0; i < n; i++) {
+  let curr_length = string_arr[i].length;
+  if (max_length <= curr_length) {
+    max_length = curr_length;
+    res_string = string_arr[i];
+  }
+}
+
+console.log(res_string);
