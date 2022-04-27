ecf036ce07f3156698d3a5a44b9cd3e7e0e0b8f1
Devjyoti29 <devjyoti.rakshit@gmail.com>
2022-04-28 00:54:36 +0530

    Control Flow- Min Occurance

--- solutions/control-flow/min-occurance.js
+++ solutions/control-flow/min-occurance.js
@@ -1,21 +1,21 @@
 let fs = require("fs");
-let data = fs.readFileSync(0, 'utf-8');
+let data = fs.readFileSync(0, "utf-8");
 let idx = 0;
-data = data.split('\n');
+data = data.split("\n");
 
 function readLine() {
-	idx++;
-	return data[idx - 1];
+  idx++;
+  return data[idx - 1];
 }
-let n=parseInt(readLine())
-let target=readLine()
-let counter=1
-for(let i=1;i<n;i++){
-	if(target==readLine()){
-		counter++
-	}
+let n = parseInt(readLine());
+let target = readLine();
+let counter = 1;
+for (let i = 1; i < n; i++) {
+  if (target == readLine()) {
+    counter++;
+  }
 }
-console.log(counter)
+console.log(counter);
 
 // -------- Do NOT edit anything above this line ----------
 // Use readLine() for taking input, it will read one line of from the input  and is stored in string format
