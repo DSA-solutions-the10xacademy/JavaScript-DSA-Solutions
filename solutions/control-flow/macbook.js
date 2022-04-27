c94b2772a98b70644369bc7b16944068af9c6441
Abhishek Ranjan <abhishekranjan0505@gmail.com>
2022-04-27 06:45:54 +0530

    Control Flow - Macbook

---
+++ solutions/control-flow/macbook.js
@@ -0,0 +1,28 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, "utf-8");
+let idx = 0;
+data = data.split("\n");
+
+function readLine() {
+  idx++;
+  return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
+
+let n = parseInt(readLine());
+let arr = [];
+for (let i = 0; i < n; i++) {
+  arr.push(parseInt(readLine()));
+}
+
+let sum = 0;
+
+for (let i = 0; i < n; i++) {
+  if (arr[i] > 0) {
+    sum += arr[i];
+  }
+}
+
+console.log(sum);
