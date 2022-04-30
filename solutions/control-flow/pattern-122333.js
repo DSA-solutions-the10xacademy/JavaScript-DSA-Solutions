f1975c1e5a52c1624e08ee5095bd1a9016eaac49
Abhishek Ranjan <abhishekranjan0505@gmail.com>
2022-04-30 11:59:24 +0530

    Control Flow - Pattern-122333

---
+++ solutions/control-flow/pattern-122333.js
@@ -0,0 +1,22 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, "utf-8");
+let idx = 0;
+data = data.split("\n");
+
+function readLine() {
+  idx++;
+  return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
+
+let n = parseInt(readLine());
+
+for (let i = 1; i <= n; i++) {
+  let column = "";
+  for (let j = 1; j <= i; j++) {
+    column += i;
+  }
+  console.log(column);
+}
