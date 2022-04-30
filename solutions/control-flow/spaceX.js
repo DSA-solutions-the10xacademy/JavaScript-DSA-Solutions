44c901e60e1bd042f9fac131c56824f1cb111213
Abhishek Ranjan <abhishekranjan0505@gmail.com>
2022-04-30 11:56:04 +0530

    Control Flow - SpaceX

---
+++ solutions/control-flow/spaceX.js
@@ -0,0 +1,26 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, "utf-8");
+let idx = 0;
+data = data.split("\n");
+
+function readLine() {
+  idx++;
+  return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
+
+let string = readLine();
+
+let modified_string = "";
+
+for (let i = 0; i < string.length; i++) {
+  if (string[i] == " ") {
+    modified_string += "spaceX";
+  } else {
+    modified_string += string[i];
+  }
+}
+
+console.log(modified_string);
