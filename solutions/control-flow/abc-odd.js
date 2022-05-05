d60f28a4bdd841eb97da069b942796864fcf7d13
prateek-mehra <63323034+prateek-mehra@users.noreply.github.com>
2022-05-05 12:44:36 +0530

    Control Flow - Abc Odd (#423)
    * created abc-odd.js
    
    * used let for variable declaration
    
    * changed the folder name according to the naming conventions
    
    * Update abc-odd.js
    
    Co-authored-by: LVSsandeepVarma <88339626+LVSsandeepVarma@users.noreply.github.com>

---
+++ solutions/control-flow/abc-odd.js
@@ -0,0 +1,23 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+    idx++;
+    return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
+
+let aAndb = readLine().split(" ");
+
+let a = parseInt(aAndb[0]);
+let b = parseInt(aAndb[1]);
+
+if ((a * b) % 2 === 0) {
+    console.log("No");
+} else {
+    console.log("Yes");
+}
