f5c374e7bb49f21eefdea787ca02a7c96a08f215
DevratAgarwal <81330019+DevratAgarwal@users.noreply.github.com>
2022-05-05 14:05:27 +0530

    Create perimeter-of-a-rectangle.js

---
+++ solutions/control-flow/perimeter-of-a-rectangle.js
@@ -0,0 +1,20 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+    idx++;
+    return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
+let length = parseInt(readLine());
+let breadth = parseInt(readLine());
+if (length > 0 && breadth > 0) {
+    console.log(2 * (length + breadth));
+}
+else {
+    console.log(0);
+}
