8d3b1ed6c8ef06940748a637d2a354412c82ec6a
DevratAgarwal <81330019+DevratAgarwal@users.noreply.github.com>
2022-05-12 16:57:28 +0530

    Arrays - len of last word (#312)
    * Arrays - len of last word
    
    * Adding file extension and refactoring for clarity
    
    Co-authored-by: Sailesh Ramayanam <33143312+sailesh-ramayanam@users.noreply.github.com>

---
+++ solutions/arrays/len-of-last-word.js
@@ -0,0 +1,15 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+  idx++;
+  return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
+let inputArray = readLine().split(" ");
+let lastWord = inputArray[inputArray.length - 1];
+console.log(lastWord.length);
