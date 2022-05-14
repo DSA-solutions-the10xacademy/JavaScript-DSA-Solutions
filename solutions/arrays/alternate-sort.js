377fd735bee8fd9aa6b493508e0cdb6ee0f08518
DevratAgarwal <81330019+DevratAgarwal@users.noreply.github.com>
2022-05-14 10:22:38 +0530

    Update alternate-sort.js

--- solutions/arrays/alternate-sort.js
+++ solutions/arrays/alternate-sort.js
@@ -4,58 +4,59 @@ let idx = 0;
 data = data.split('\n');
 
 function readLine() {
-    idx++;
-    return data[idx - 1];
+	idx++;
+	return data[idx - 1];
 }
 
 // -------- Do NOT edit anything above this line ----------
 // Use readLine() for taking input, it will read one line of from the input  and is stored in string format
 let testCases = parseInt(readLine());
 while (testCases > 0) {
-    let arr = readLine().split(" ").map(Number);
-    let evenPositionArray = [];
-    let oddPositionArray = [];
-    // storing values in even and odd indexing based arrays
-    for (let index = 0; index < arr.length; index++) {
-        if (index % 2 === 0) {
-            evenPositionArray.push(arr[index]);
-        }
-        else {
-            oddPositionArray.push(arr[index]);
-        }
-    }
-    // sorting odd index based array in ascending order using bubble sort
-    for (let currIndex = 0; currIndex < oddPositionArray.length; currIndex++) {
-        for (let nextIndex = currIndex + 1; nextIndex < oddPositionArray.length; nextIndex++) {
-            if (oddPositionArray[currIndex] > oddPositionArray[nextIndex]) {
-                let temporary = oddPositionArray[nextIndex];
-                oddPositionArray[nextIndex] = oddPositionArray[currIndex];
-                oddPositionArray[currIndex] = temporary;
-            }
-        }
-    }
-    //sorting even based array in descending order using bubble sort
-    for (let currIndex = 0; currIndex < evenPositionArray.length; currIndex++) {
-        for (let nextIndex = currIndex + 1; nextIndex < evenPositionArray.length; nextIndex++) {
-            if (evenPositionArray[currIndex] < evenPositionArray[nextIndex]) {
-                let temporary = evenPositionArray[nextIndex];
-                evenPositionArray[nextIndex] = evenPositionArray[currIndex];
-                evenPositionArray[currIndex] = temporary;
-            }
-        }
-    }
-    // generating answer array teking values from both even and odd indexs based arrays respectively.
-    let answer = [];
-    for (let index = 0; index < arr.length; index++) {
-        if (index % 2 == 0) {
-            answer.push(evenPositionArray[parseInt(index / 2)]);
-        }
-        else {
-            answer.push(oddPositionArray[parseInt(index / 2)]);
-        }
-    }
-    for (let index = 0; index < answer.length; index++) {
-        console.log(answer[index]);
-    }
-    testCases = testCases - 1;
+	let arr = readLine().split(" ").map(Number);
+	let evenPositionArray = [];
+	let oddPositionArray = [];
+	// storing values in even and odd indexing based arrays
+	for (let index = 0; index < arr.length; index++) {
+		if (index % 2 === 0) {
+			evenPositionArray.push(arr[index]);
+		}
+		else {
+			oddPositionArray.push(arr[index]);
+		}
+	}
+	// sorting odd index based array in ascending order using insertion sort
+	for (let currIndex = 0; currIndex < oddPositionArray.length; currIndex++) {
+		let key = oddPositionArray[currIndex];
+		let prevIndex = currIndex - 1;
+		while (prevIndex >= 0 && oddPositionArray[prevIndex] > key) {
+			oddPositionArray[prevIndex + 1] = oddPositionArray[prevIndex];
+			prevIndex = prevIndex - 1;
+		}
+		oddPositionArray[prevIndex + 1] = key;
+	}
+
+	//sorting even based array in descending order using insertion sort
+	for (let currIndex = 0; currIndex < evenPositionArray.length; currIndex++) {
+		let key = evenPositionArray[currIndex];
+		let prevIndex = currIndex - 1;
+		while (prevIndex >= 0 && evenPositionArray[prevIndex] < key) {
+			evenPositionArray[prevIndex + 1] = evenPositionArray[prevIndex];
+			prevIndex = prevIndex - 1;
+		}
+		evenPositionArray[prevIndex + 1] = key;
+	}
+	// generating answer array teking values from both even and odd indexs based arrays respectively.
+	let answer = [];
+	for (let index = 0; index < arr.length; index++) {
+		if (index % 2 == 0) {
+			answer.push(evenPositionArray[parseInt(index / 2)]);
+		}
+		else {
+			answer.push(oddPositionArray[parseInt(index / 2)]);
+		}
+	}
+	for (let index = 0; index < answer.length; index++) {
+		console.log(answer[index]);
+	}
+	testCases = testCases - 1;
 }
