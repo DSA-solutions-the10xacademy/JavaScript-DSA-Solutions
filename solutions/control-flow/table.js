dd18cd55468947ea0695f7a2c07131070fbb93fd
Devjyoti29 <devjyoti.rakshit@gmail.com>
2022-05-19 11:11:15 +0530

    Control FLow - Table

---
+++ solutions/control-flow/table.js
@@ -0,0 +1,18 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+  idx++;
+  return data[idx - 1];
+}
+let n=parseInt(readLine())
+let arr=[]
+for(let i=1;i<=10;i++){
+	arr.push(n*i)
+}
+console.log(...arr)
+
+// -------- Do NOT edit anything above this line ----------
+// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
