a85ab6209a5dec95a5708d30b2373b683d6d1faf
DevratAgarwal <81330019+DevratAgarwal@users.noreply.github.com>
2022-06-15 21:36:33 +0530

    Create divisibility-test.js

---
+++ solutions/control-flow/divisibility-test.js
@@ -0,0 +1,24 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+    idx++;
+    return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
+let val = readLine().split(" ");
+let arraySize = parseInt(val[0]);
+let x = parseInt(val[1]);
+let arr = readLine().split(" ");
+let count = 0;
+for (let index = 0; index < arraySize; index++) {
+    arr[index] = parseInt(arr[index]);
+    if (arr[index] % x == 0) {
+        count = count + 1;
+    }
+}
+console.log(count);
