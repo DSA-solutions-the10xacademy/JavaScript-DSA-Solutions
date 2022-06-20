48028b6259a84f1444906ba2794efcf1941766a9
prateek-mehra <63323034+prateek-mehra@users.noreply.github.com>
2022-06-20 10:27:11 +0530

    Control Flow - Area of Rectangle (#415)
    * created area-of-rectangle.js
    
    * removed logical operator
    
    * Rename solutions/control flow/area-of-rectangle.js to solutions/control-flow/area-of-rectangle.js
    
    changed the folder name according to naming conventions
    
    * included let for variable declaration
    
    Co-authored-by: LVSsandeepVarma <88339626+LVSsandeepVarma@users.noreply.github.com>

---
+++ solutions/control-flow/area-of-rectangle.js
@@ -0,0 +1,24 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+  idx++;
+  return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
+
+let length = parseInt(readLine());
+let breadth = parseInt(readLine());
+
+if (length < 0) {
+  console.log(0);
+}
+else if (breadth < 0) {
+  console.log(0);
+} else {
+  console.log(length * breadth);
+}
