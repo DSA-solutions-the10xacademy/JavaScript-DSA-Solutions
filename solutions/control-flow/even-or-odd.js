0c3c3f2a9e21fa6520e21ccc048c6b472f602e9c
Saivijay57 <korvisaivijay@gmail.com>
2022-09-27 11:51:34 +0530

    control flow - Even or Odd

---
+++ solutions/control-flow/even-or-odd.js
@@ -0,0 +1,17 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+    idx++;
+    return data[idx - 1].trim();
+}
+
+// -------- Do NOT edit anything above this line ----------
+let givenInteger = parseInt(readLine());
+if(givenInteger%2==0){
+	console.log("Even");
+}else{
+	console.log("Odd");
+}
