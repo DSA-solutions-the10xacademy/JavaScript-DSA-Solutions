8f92a3d67d3eadaa56b985cb9b88dae128b328e0
Saivijay57 <korvisaivijay@gmail.com>
2022-09-29 12:15:26 +0530

    Arrays - Alternate 101

---
+++ solutions/arrays/alternate-101.js
@@ -0,0 +1,21 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+    idx++;
+    return data[idx - 1].trim();
+}
+
+// -------- Do NOT edit anything above this line ----------
+// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
+let sizeOfTheArray = parseInt(readLine());
+let array = readLine().split(" ");
+let alternateValuesSum = 0;
+for(let index = 0 ; index < sizeOfTheArray ; index++){
+	if(index % 2 === 0){
+		alternateValuesSum = alternateValuesSum + parseInt(array[index])
+	}
+}
+console.log(alternateValuesSum);
